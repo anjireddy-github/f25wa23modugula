470fe37c15add0e62a880fd2b88ae88e1a6bf8b8
Anji Reddy Modugula <modugulaanjireddy18@gmail.com>
2025-10-29 18:35:30 -0400

    endpoint modification

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var factor = 2;
+var current = 25;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  current = current * factor;
+  res.send(`Current is: ${current}`);
 });
 
 module.exports = router;
